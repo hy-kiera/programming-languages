b13ec95d08463b3a9fcf1fd93badafb710a642c8
hy-kiera <bb.kiera@gmail.com>
2020-06-10 21:22:15 +0900

    hw5

--- hw5/hw5_skel/m.ml
+++ hw5/hw5_skel/m.ml
@@ -20,7 +20,7 @@ and var = string
 exception TypeError
 
 type typ = TyInt | TyBool 
-	| TyFun of typ * typ (* t1 -> t2 *) 
+	| TyFun of typ * typ (* t1 -> t2 *)
 	| TyVar of tyvar
 and tyvar = string
 type typ_eqn = (typ * typ) list (* t1 = t2 *)
@@ -75,12 +75,81 @@ let tyvar_num = ref 0
 (* generate a fresh type variable *)
 let fresh_tyvar () = (tyvar_num := !tyvar_num + 1; (TyVar ("t" ^ string_of_int !tyvar_num)))
 
+(* my functions *)
+let get_type ty = 
+  match ty with
+  | TyInt -> "TyInt"
+  | TyBool -> "TyBool"
+  | TyFun (t1,t2) -> "TyFun"
+  | TyVar x -> "TyVar"
+
+let tyfun_first x =
+  match x with
+  TyFun (f, _) -> f
+  | _ -> raise TypeError
+
+let tyfun_second x =
+  match x with
+  TyFun (_, s) -> s
+  | _ -> raise TypeError
+
+let count_string s x =
+  let rec iter iter_str cnt =
+    match (s, x) with
+    (_, "") -> 0
+    | (_, _) ->
+      if iter_str = ((String.length s) - (String.length x)) then 
+        if (String.sub s iter_str (String.length x)) = x then (cnt + 1)
+        else cnt
+      else
+        if (String.sub s iter_str (String.length x)) = x then iter (iter_str + 1) (cnt + 1)
+        else iter (iter_str + 1) cnt
+    in iter 0 0;;
+
+exception MyError
+
+(* TODO *)
 let rec gen_equations : TEnv.t -> exp -> typ -> typ_eqn 
-=fun tenv e ty -> raise TypeError
+=fun tenv e ty ->
+  match e with
+  TRUE | FALSE -> [(ty, TyBool)]
+  | CONST c -> [(ty, TyInt)]
+  | VAR x -> let tyX = (TEnv.find tenv x) in [(ty, tyX)]
+  | ADD (e1, e2) | SUB (e1, e2) | MUL (e1, e2) | DIV (e1, e2) -> [(ty, TyInt)] @ (gen_equations tenv e1 TyInt) @ (gen_equations tenv e2 TyInt)
+  | ISZERO e -> [(ty, TyBool)] @ (gen_equations tenv e TyInt)
+  | READ -> [(ty, TyInt)]
+  | IF (e1, e2, e3) -> (gen_equations tenv e1 TyBool) @ (gen_equations tenv e2 ty) @ (gen_equations tenv e3 ty)
+  | LET (x, e1, e2) -> let alpha = fresh_tyvar() in
+                        (gen_equations tenv e1 alpha) @ (gen_equations (TEnv.extend (x, alpha) tenv) e2 ty)
+  | LETREC (f, x, e1, e2) -> let alpha1 = fresh_tyvar() in
+                              let alpha2 = fresh_tyvar() in
+                                let t = (gen_equations (TEnv.extend (f, TyFun (alpha2, alpha1)) tenv) e2 ty) in
+                                  let new_tenv = (TEnv.extend (x, alpha2) tenv) in
+                                    (gen_equations (TEnv.extend (f, TyFun (alpha2, alpha1)) new_tenv) e1 alpha1) @ t
+  | PROC (x, e) -> let alpha1 = fresh_tyvar() in
+                    let alpha2 = fresh_tyvar() in
+                      [ty, TyFun (alpha1, alpha2)] @ (gen_equations (TEnv.extend (x, alpha1) tenv) e alpha2)
+  | CALL (e1, e2) -> let alpha = fresh_tyvar() in
+                      (gen_equations tenv e1 (TyFun (alpha, ty))) @ (gen_equations tenv e2 alpha)
 
-
+(* TODO *)
 let solve : typ_eqn -> Subst.t
-=fun eqns -> raise TypeError 
+=fun eqns -> let rec unify x y sub =
+              if string_of_type x = string_of_type y then sub (* int = int, bool = bool, alpha = alpha *)
+              else if (get_type x = "TyVar") then
+                      if count_string (string_of_type y) (string_of_type x) > 0 then raise TypeError (* occurence check *)
+                      else Subst.extend (string_of_type x) y sub
+              else if ((get_type x = "TyInt" || get_type x = "TyBool" || get_type x = "TyFun") && get_type y = "TyVar") then unify y x sub
+              else if (get_type x = "TyFun" && get_type y = "TyFun") then let s' = unify (tyfun_first x) (tyfun_first y) sub in
+                                                                        let s'' = unify (Subst.apply (tyfun_second x) s') (Subst.apply (tyfun_second y) s') s' in
+                                                                          s''
+              else raise TypeError
+            in let rec unifyall eqns sub =
+                match eqns with
+                [] -> sub
+                | (x,y)::t -> let s' = unify (Subst.apply x sub) (Subst.apply y sub) sub in
+                                unifyall t s'
+              in unifyall eqns Subst.empty
 
 
 (* typeof: Do not modify this function *)
--- hw5/hw5_skel/p.ml
+++ hw5/hw5_skel/p.ml
@@ -3,13 +3,57 @@ open M
 (* exp environment : var -> exp *)
 module EEnv = struct
   type t = var -> exp
-  let empty = fun _ -> raise (Failure "Exp Env is empty")
+  (* let empty = fun _ -> raise (Failure "Exp Env is empty") *)
+  let empty = fun _ -> FALSE
   let extend (x,t) eenv = fun y -> if x = y then t else (eenv y)
   let find eenv x = eenv x
 end
 
+(* my exception *)
+exception NotImplemented
+
 let expand: exp -> exp 
-= fun exp -> raise (Failure "NotImplemented")		
+= fun exp ->
+  let rec isx exp x =
+    match exp with
+    TRUE | FALSE -> false
+    | CONST c -> false
+    | VAR x' -> if x' = x then true
+                else false
+    | ADD (e1, e2) | SUB (e1, e2) | MUL (e1, e2) | DIV (e1, e2) -> (isx e1 x) || (isx e2 x)
+    | ISZERO e -> isx e x
+    | READ -> false
+    | IF (e1, e2, e3) -> (isx e1 x) || (isx e2 x) || (isx e3 x)
+    | LET (x', e1, e2)-> if x' = x then true
+                          else (isx e1 x) || (isx e2 x)
+    | LETREC (f, x', e1, e2) -> if x' = x then true
+                                else (isx e1 x) || (isx e2 x)
+    | PROC (x', e) -> if x' = x then true
+                        else isx e x
+    | CALL (e1, e2) -> (isx e1 x) || (isx e2 x)
+  in
+    let rec trans exp eenv =
+      match exp with
+      TRUE | FALSE -> exp
+      | CONST c -> exp
+      | VAR x -> let isexist = EEnv.find eenv x in
+                  if isexist = FALSE then exp
+                  else isexist
+      | ADD (e1, e2) -> ADD ((trans e1 eenv), (trans e2 eenv))
+      | SUB (e1, e2) -> SUB ((trans e1 eenv), (trans e2 eenv))
+      | MUL (e1, e2) -> MUL ((trans e1 eenv), (trans e2 eenv))
+      | DIV (e1, e2) -> DIV ((trans e1 eenv), (trans e2 eenv))
+      | ISZERO e -> ISZERO (trans e eenv)
+      | READ -> exp
+      | IF (e1, e2, e3) -> IF ((trans e1 eenv), (trans e2 eenv), (trans e3 eenv))
+      | LET (x, e1, e2) -> let new_eenv = (EEnv.extend (x, (trans e1 eenv)) eenv) in
+                            if isx e2 x then trans e2 new_eenv
+                            else LET (x, (trans e1 new_eenv), (trans e2 new_eenv))
+      | LETREC (f, x, e1, e2) -> exp
+      | PROC (x, e) -> PROC (x, (trans e eenv))
+      | CALL (e1, e2) -> CALL ((trans e1 eenv), (trans e2 eenv))
+    in trans exp EEnv.empty
+
 
 (* typeof: Do not modify this function *)
 let typeof : exp -> typ 
